7d522825cb69e82769a8654e469794d36485835d
sd-fritze <pcneuverdrahter+github@gmail.com>
2013-03-29 15:14:14 +0100

    changed compressor

--- src/minecraft/de/tenaciousnetwork/airmod/BlockCompressor.java
+++ src/minecraft/de/tenaciousnetwork/airmod/BlockCompressor.java
@@ -1,16 +1,51 @@
 package de.tenaciousnetwork.airmod;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.Icon;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
 import net.minecraftforge.liquids.ILiquidTank;
 import net.minecraftforge.liquids.LiquidStack;
 
 public class BlockCompressor extends Block 
 {
+	private Icon[] tex = new Icon[4];
+	/* (non-Javadoc)
+	 * @see net.minecraft.block.Block#registerIcons(net.minecraft.client.renderer.texture.IconRegister)
+	 */
+	@Override
+	public void registerIcons(IconRegister par1) {
+		tex[0] = par1.registerIcon("airmod:mbottom");
+		tex[1] = par1.registerIcon("airmod:mside");
+		tex[2] = par1.registerIcon("airmod:compf_unlit");
+		tex[3] = par1.registerIcon("airmod:compf_lit");
+	}
+
+	/* (non-Javadoc)
+	 * @see net.minecraft.block.Block#getBlockTextureFromSideAndMetadata(int, int)
+	 */
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Icon getBlockTextureFromSideAndMetadata(int side, int meta) 
+	{
+		if (side == 0){
+			return tex[0];
+		}
+		if (side == (meta&7)){
+			if ((meta&8)>0) {
+				return tex[3];
+			} else
+			return tex[2];
+		}
+		return tex[1];
+	}
+
 	public BlockCompressor(int par1, Material par2Material) {
 		super(par1, par2Material);
 		// TODO Auto-generated constructor stub
@@ -20,5 +55,6 @@ public class BlockCompressor extends Block
     {
         return new TileEntityCompressor();
     }
+
     
 }
